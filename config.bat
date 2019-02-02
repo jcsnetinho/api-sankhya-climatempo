25ef185bb6b4c6e376b23a3747b4a44d4cc61911
Daniel <danielaraujos@live.com>
2019-02-02 18:32:47 -0200

    Create script bat for run application jar

---
+++ config.bat
@@ -0,0 +1,6 @@
+@echo off
+cls
+echo -------------------------
+echo        Gerenciamento
+echo -------------------------
+java -Xmx200m -jar src/main/java/api_config.jar
---
+++ script.bat
@@ -0,0 +1,6 @@
+@echo off
+cls
+echo -------------------------
+echo        Script de Integracao
+echo -------------------------
+java -Xmx200m -jar src/main/java/api_clima.jar
